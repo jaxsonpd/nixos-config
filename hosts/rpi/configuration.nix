b8940196e4c2fcdf68dfb206e78b267352414eb3
Jack Duignan <56944950+jaxsonpd@users.noreply.github.com>
2025-10-09 08:45:01 +1300

    Update configuration.nix

--- hosts/rpi/configuration.nix
+++ hosts/rpi/configuration.nix
@@ -7,11 +7,5 @@
 
   networking.hostName = "rpi-jd";
 
-  users.users.jackd = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    shell = pkgs.zsh;
-  };
-
   system.stateVersion = "25.05";
 }

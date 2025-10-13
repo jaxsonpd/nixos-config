ed8c4cd74b389b915a95fbd6617a1348994ee0b7
Jack Duignan <jackpduignan@gmail.com>
2025-10-13 16:10:13 +1300

    Add rtl support

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,11 +1,13 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
+  hardware.rtl-sdr.enable = true;
+
   users.users.jackd = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "plugdev" ];
     shell = pkgs.nushell;
-    hashedPassword = "$6$1vkTCTcl2QcGUykM$4LJmHBM7sIUkFmrUCR/P9YeObsOzDZ7GUrMwOhdGJoIKgYY.ESVEyBoJSyLMO4QCtKTQ3J7IVwR8QC5eaW7Og."; 
+    hashedPassword = "$6$1vkTCTcl2QcGUykM$4LJmHBM7sIUkFmrUCR/P9YeObsOzDZ7GUrMwOhdGJoIKgYY.ESVEyBoJSyLMO4QCtKTQ3J7IVwR8QC5eaW7Og.";
   };
 }
 
